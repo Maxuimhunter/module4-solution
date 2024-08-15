33eae577c2924f984569c3dea418443796892717
Anthony <gathukiaanthony@gmail.com>
2024-08-15 11:10:03 +0300

    This are the two files that i edited Thank you for the course coursera
        - modified:   index.html
    	- modified:   script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 //
 (function () {
 
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Max", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
   // Loop over the names array and say either 'Hello' or "Good Bye"
   for (var i = 0; i < names.length; i++) {
